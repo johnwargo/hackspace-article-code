8975523381fb3214124446ff9bca5cb209a12c2d
John M. Wargo <johnwargo@users.noreply.github.com>
2017-12-31 15:51:28 -0500

    updated comments in 03-01

--- arduino-dht22-1/arduino-dht22-1.ino
+++ arduino-dht22-1/arduino-dht22-1.ino
@@ -1,19 +1,20 @@
 // Example testing sketch for various DHT humidity/temperature sensors
 // Written by ladyada, public domain
-// Additional comments by John M. Wargo
+
+// Additional comments, and slight updates by John M. Wargo
 
 #include "DHT.h"
 
 #define DHTPIN 2     // what digital pin we're connected to
 
 // Uncomment the line for the specific hardware you're using
-//commenting out the other lines
+// commenting out the remaining lines
 //#define DHTTYPE DHT11   // DHT 11
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Initialize DHT sensor library for the specific hardware configuration
-// we're using
+// Create DHT sensor object for the specific hardware
+// configuration we're using
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
@@ -27,28 +28,36 @@ void setup() {
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
+  // Wait two seconds between measurements
   delay(2000);
 
+  // ==========================================================================
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  // ==========================================================================
+  // Read the humidity
   float h = dht.readHumidity();
-  // Read temperature as Celsius (the default)
+  // Read temperature in degrees Celsius (the default)
   float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
+  // Another way to do this is:
+  // float t = dht.readTemperature(false);
+  // Read temperature in degrees Fahrenheit 
+  // (isFahrenheit = true)
   float f = dht.readTemperature(true);
 
-  // Check if any reads failed and exit early (to try again).
+  // Make sure the sketch was able to read values from the sensor before
+  // continuing. the return exits the loop (try again)
   if (isnan(h) || isnan(t) || isnan(f)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  // Compute heat index in Fahrenheit (the default)
+  // Compute heat index in degrees Fahrenheit (the default)
   float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
+  // Compute heat index in degrees Celsius (isFahreheit = false)
   float hic = dht.computeHeatIndex(t, h, false);
 
+  // Write the resulting data (properly formatted) to the serial port
   Serial.print("Humidity: ");
   Serial.print(h);
   Serial.print("%\t");
