db763cabfc37df265278491321ccd48e799b6680
John M. Wargo <johnwargo@users.noreply.github.com>
2018-01-01 17:07:40 -0500

    updated Mon 01/01/2018 17:07:40.68

--- arduino-dht22-1/arduino-dht22-1.ino
+++ arduino-dht22-1/arduino-dht22-1.ino
@@ -72,3 +72,4 @@ void loop() {
   Serial.print(hif);
   Serial.println("F");
 }
+
--- arduino-dht22-2/arduino-dht22-2.ino
+++ arduino-dht22-2/arduino-dht22-2.ino
@@ -15,14 +15,16 @@
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 // constants used to determine the trigger direction
-// greater or lessa than the trigger value
+// greater or less than the trigger value
 const int LESS_THAN = 0;
 const int GREATER_THAN = 1;
 
-// set this value to the temperature you want to trigger the LED on/off
-const int TRIGGER_TEMP = 70; // Degrees F
+// set this value to the temperature you want to
+// trigger the LED on/off
+const int TRIGGER_TEMP = 72; // Degrees F
 // Trigger direction: trigger the LED when the temp is...
 const int TRIGGER_DIRECTION = GREATER_THAN;
+//const int TRIGGER_DIRECTION = LESS_THAN;
 // Valid options are GREATER_THAN or LESS_THAN
 
 // Create DHT sensor object for the specific hardware
@@ -35,13 +37,15 @@ void setup() {
   Serial.begin(9600);
   Serial.println("DHT Temperature Monitor");
 
+  Serial.println("Validating sketch configuration");
   // check to make sure trigger direction is a valid value
   if ((TRIGGER_DIRECTION < LESS_THAN) || (TRIGGER_DIRECTION > GREATER_THAN)) {
     Serial.println("Invalid value for TRIGGER_DIRECTION, please fix the sketch and try again");
-    // The code is broken, so loop inifinitely
+    // The code is broken, so loop infinitely
     while (true);
     // the sketch gets stuck here, and never blinks the LED.
   }
+  Serial.println("Configuration validated");
 
   // configure the LED pin for output mode
   pinMode(LEDPIN, OUTPUT);
@@ -51,12 +55,9 @@ void setup() {
 
   // blink the LED twice so you can tell the sketch is working.
   blinkLED(2, 250);
-
 }
 
 void loop() {
-
-  // boolean ledStatus;
 
   // Wait two seconds between measurements
   delay(2000);
@@ -73,43 +74,29 @@ void loop() {
   // or you can use this alternate method:
   //float currTemp = dht.readTemperature();
 
-  // Make sure the sketch was able to read values from the sensor before
-  // continuing. The return exits the loop (try again)
+  Serial.print("Temperature: ");
+  Serial.print(currTemp);
+  Serial.println(" degrees");
+
+  // Make sure the sketch was able to read values 
+  // from the sensor before continuing. 
+  // The return exits the loop (try again)
   if (isnan(currTemp))  {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  //  // If we got here, we've got a valid temp reading, so we can do something with it.
-  //  // initialize our LED status to false (off)
-  //  ledStatus = false;
-  //  // now check to see if it should be on
-  //  if (TRIGGER_DIRECTION < GREATER_THAN) {
-  //    // Then we're doing a less than option, so check to
-  //    // see if the current temp is less than the trigger temp
-  //    ledStatus = (currTemp < TRIGGER_TEMP);
-  //  } else {
-  //    // Otherwise, we're doing a greater than option, so check to
-  //    // see if the current temp is greater than the trigger temp
-  //    ledStatus = (currTemp > TRIGGER_TEMP);
-  //  }
-  //
-  //  // Now set the LED power output based on ledStatus
-  //  if (ledStatus) {
-  //    // Turn the LED on
-  //    digitalWrite(LEDPIN, HIGH);
-  //  } else {
-  //    // Turn the LED off
-  //    digitalWrite(LEDPIN, LOW);
-  //  }
-
+  // If we got here, we've got a valid temp reading, 
+  // so we can do something with it.
   if (TRIGGER_DIRECTION < GREATER_THAN) {
     // Then we're doing a less than option, so check to
-    // see if the current temp is less than the trigger temp
+    // see if the current temp is less than the trigger temp and 
+    // write the appropriate value to the output pin
     digitalWrite(LEDPIN, (currTemp < TRIGGER_TEMP) ? HIGH : LOW);
   } else {
     // Otherwise, we're doing a greater than option, so check to
-    // see if the current temp is greater than the trigger temp
+    // see if the current temp is greater than the trigger temp and
+    // write the appropriate value to the output pin
     digitalWrite(LEDPIN, (currTemp > TRIGGER_TEMP) ? HIGH : LOW);
   }
 
